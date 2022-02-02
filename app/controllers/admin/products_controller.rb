bcb13e8a651320afc47cd1a64eaf250502058fdd
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-02 15:34:11 -0500

    Added auth to admin features

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with :name => ENV['SUPER_SECURE_USERNAME'], :password => ENV['SUPER_SECURE_PASSWORD']
 
   def index
     @products = Product.order(id: :desc).all
