bba2cf3123b48ba4dce1c3aa748e928cf7dbcc86
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-03 13:36:49 -0500

    Added session controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -13,14 +13,21 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  # Admin routes
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :destroy, :show]
   end
 
+  # Signup routes
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+  # Login routes
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
